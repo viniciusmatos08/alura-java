117815139f8f55670cf156512acfb2e04e45553e
viniciusmatos08 <viniciusmatos08@gmail.com>
2020-10-06 23:12:33 -0300

    Modulo 6 - Aplicando Generics no ArrayLis

--- Modulo 6/src/br/com/bytebank/banco/teste/util/Teste.java
+++ Modulo 6/src/br/com/bytebank/banco/teste/util/Teste.java
@@ -8,7 +8,7 @@ import br.com.bytebank.banco.modelo.ContaCorrente;
 public class Teste {
     public static void main(String[] args) {
 
-        ArrayList lista = new ArrayList();
+        ArrayList<Conta> lista = new ArrayList<Conta>();
 
         Conta cc = new ContaCorrente(22, 11);
         lista.add(cc);
@@ -18,7 +18,7 @@ public class Teste {
 
         System.out.println("Tamanho: " + lista.size());
 
-        Conta ref = (Conta) lista.get(0);
+        Conta ref = lista.get(0);
         System.out.println(ref.getNumero());
 
         lista.remove(0);
@@ -37,7 +37,7 @@ public class Teste {
 
         System.out.println("----------");
 
-        for(Object oRef : lista) {
+        for(Conta oRef : lista) {
             System.out.println(oRef);
         }
 
