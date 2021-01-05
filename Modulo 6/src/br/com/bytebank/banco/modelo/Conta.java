792bee1461c557e41cc5278a44d55bc33b357e2c
viniciusmatos08 <viniciusmatos08@gmail.com>
2021-01-05 18:02:53 -0300

    Modulo 6 - Sobrescrevendo o metodo equals

--- Modulo 6/src/br/com/bytebank/banco/modelo/Conta.java
+++ Modulo 6/src/br/com/bytebank/banco/modelo/Conta.java
@@ -83,4 +83,20 @@ public abstract class Conta{
 		return "Numero "+this.numero+", Agencia "+this.agencia;
 	}
 	
+	@Override
+	public boolean equals(Object ref){
+
+	    Conta outra = (Conta) ref;
+
+	    if(this.agencia != outra.agencia){
+	        return false;
+	    }
+
+	    if(this.numero != outra.numero){
+	        return false;
+	    }
+
+	    return true;
+	}
+	
 }
