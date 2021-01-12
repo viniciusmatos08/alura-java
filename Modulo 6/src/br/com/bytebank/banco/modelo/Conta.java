a05a5fadd488cf05e02ad524317caf5b4c17902d
viniciusmatos08 <viniciusmatos08@gmail.com>
2021-01-12 00:05:30 -0300

    Modulo 6 - Ordenando listas usando sort e como parametro Comparator, tambem Comparable para ordem natual

--- Modulo 6/src/br/com/bytebank/banco/modelo/Conta.java
+++ Modulo 6/src/br/com/bytebank/banco/modelo/Conta.java
@@ -6,7 +6,7 @@ package br.com.bytebank.banco.modelo;
 * @version 0.1
 * 
 */
-public abstract class Conta{
+public abstract class Conta implements Comparable<Conta>{
 	protected double saldo;
 	private int agencia;
 	private int numero;
@@ -97,6 +97,11 @@ public abstract class Conta{
 	    }
 
 	    return true;
+	}
+	
+	@Override
+	public int compareTo(Conta outra) {
+	    return Double.compare(this.saldo, outra.saldo);
 	}
 	
 }
