572e2d376d411d2032f001dc4d68ee67e3764366
viniciusmatos08 <viniciusmatos08@gmail.com>
2021-01-26 23:43:50 -0300

    Collections - Usando a collection Set na pratica

--- formacao-java-e-orientacao-a-objetos/JavaCollections/src/br/com/alura/Curso.java
+++ formacao-java-e-orientacao-a-objetos/JavaCollections/src/br/com/alura/Curso.java
@@ -2,13 +2,16 @@ package br.com.alura;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Curso {
 	
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new ArrayList<Aula>();
+	private Set<Aluno> alunos = new HashSet<>();
 	
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
@@ -44,6 +47,14 @@ public class Curso {
 	@Override
 	public String toString() {
 	    return "[Curso:"+ this.getNome()+",tempo total:"+ this.getTempoTotal()+ ", aulas: ["+ this.aulas+"] ]";
+	}
+	
+	public void matricula(Aluno aluno){
+	    this.alunos.add(aluno);
+	}
+	
+	public Set<Aluno> getAlunos() {
+	    return Collections.unmodifiableSet(alunos);
 	}
 
 }
