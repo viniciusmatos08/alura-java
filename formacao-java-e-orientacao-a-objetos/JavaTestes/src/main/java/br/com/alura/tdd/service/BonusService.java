55b0e3aeaf372985bd923e0fadcd1fa7f7315abf
vinicius_matos <vinicius_matos@hst.com.br>
2021-07-15 12:14:21 -0300

    JavaTestes - Criando testes com JUnit para validar logica de bonificação

--- formacao-java-e-orientacao-a-objetos/JavaTestes/src/main/java/br/com/alura/tdd/service/BonusService.java
+++ formacao-java-e-orientacao-a-objetos/JavaTestes/src/main/java/br/com/alura/tdd/service/BonusService.java
@@ -1,17 +1,23 @@
 package br.com.alura.tdd.service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import br.com.alura.tdd.modelo.Funcionario;
 
 public class BonusService {
 
 	public BigDecimal calcularBonus(Funcionario funcionario) {
+		
+		//Calculo que define o valor do bonus, sendo que ele é igual a 10% do valor do salario
+		//Caso o bonus seja maior que 1000, o funcionario não é bonificado.
 		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
 		if (valor.compareTo(new BigDecimal("1000")) > 0) {
 			valor = BigDecimal.ZERO;
 		}
-		return valor;
+		
+		//Define que o valor tera 2 casas decimais e sera arredondado para cima
+		return valor.setScale(2, RoundingMode.HALF_UP);
 	}
 
 }
